0fa88feab108a5dd91687ee65eefaf1000c13dc9
Oscar Legorreta <Oscar_Legorreta@epam.com>
2018-06-06 15:23:53 -0500

    Added SignalR

---
+++ FaceGateway.Web/Controllers/FaceRecognitionController.cs
@@ -0,0 +1,20 @@
+﻿using FaceGateway.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace FaceGateway.Web.Controllers
+{
+    public class FaceRecognitionController : ApiController
+    {
+        [HttpPost]
+        public async Task<IHttpActionResult> Alert(ImageRecognitionMessage message) {
+
+            return Ok(message);
+        }
+    }
+}
---
+++ FaceGateway.Web/Models/ImageRecognitionMessage.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FaceGateway.Web.Models
+{
+    public class ImageRecognitionMessage
+    {
+        public Guid CamId { get; set; }
+        [JsonProperty("in")]
+        public string FileName { get; set; }
+        [JsonProperty("t")]
+        public long TimeStamp { get; set; }
+        public IEnumerable<Guid> FaceIds { get; set; }
+    }
+}
