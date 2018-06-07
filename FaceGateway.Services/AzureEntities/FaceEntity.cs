63b819873ad44e6ea04c3735a0222431b7c0619a
Oscar Legorreta <Oscar_Legorreta@epam.com>
2018-06-07 15:56:33 -0500

    Added faces table access

---
+++ FaceGateway.Services/AzureEntities/FaceEntity.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceGateway.Services.AzureEntities
+{
+   
+    public class FaceEntity : TableEntity
+    {
+        public void SetPartitionKey() {
+            this.PartitionKey = Guid.NewGuid().ToString();
+        }
+        public void SetRowKey() => RowKey = Guid.NewGuid().ToString();
+        public Guid FaceId { get; set; }
+        public string Name { get; set; }
+        public string TrainingImageFile { get; set; }
+    }
+}
--- FaceGateway.Services/ConcreteServices/FacesService.cs
+++ FaceGateway.Services/ConcreteServices/FacesService.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using FaceGateway.Services.AzureEntities;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +13,24 @@ namespace FaceGateway.Services.ConcreteServices
 {
     public class FacesService : IFacesService
     {
+        private string Combine(IEnumerable<string> filters) {
+            if (filters.Count() == 1) {
+                return filters.First();
+            }
+            return TableQuery.CombineFilters(filters.First(),TableOperators.Or,Combine(filters.Skip(1)));
+        }
         public IEnumerable<Face> GetFaces(IEnumerable<Guid> faceIds)
         {
-            
-            return faceIds.Select(f=>new Face());
+            if (!faceIds.Any()) return new List<Face>();
+            var storageAcct = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["AZURE-TABLES-CONN-STR"]);
+            var tableClient = storageAcct.CreateCloudTableClient();
+            var table = tableClient.GetTableReference("Faces");
+            var query = new TableQuery<FaceEntity>();
+            var wheres = faceIds.Select( fid => TableQuery.GenerateFilterCondition("FaceId",QueryComparisons.Equal,fid.ToString()) );
+            var filter = query.Where(Combine(wheres));
+
+//            return faces.Select(f => new Face { Name = f.Name, TrainingImageFile = f.TrainingImageName });
+            return table.ExecuteQuery(filter).Select(f => new Face { Name = f.Name, TrainingImageFile = f.TrainingImageFile });
         }
     }
 }
